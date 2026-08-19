58ec657afdb571d1b51ac79c850caac53458afa5
Nitin Gosai <nitingosai.in@gmail.com>
2026-08-19 09:24:02 +0530

    Build a Counter Web App using HTML, CSS and JavaScript

---
+++ 03_javaScript/counter-app/logics.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+let countEl = document.getElementById("count-el");
+let savedNum = document.getElementById("saved-num");
+let date = document.getElementById("date");
+
+const now = new Date();
+const time = now.toLocaleTimeString();
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function saveCount() {
+  savedNum.textContent = count;
+  date.textContent = time;
+
+  count = 0
+  countEl.textContent = count
+}
